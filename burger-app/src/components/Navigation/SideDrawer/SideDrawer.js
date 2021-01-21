bf815a12333bdd3f483099d08fb3174fec05b17d
Joshua Lewis <joshualewis@adminisatorsmbp.home>
2021-01-20 22:09:37 -0500

    First Commit

---
+++ burger-app/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigaitonItems/NavigationItems";
+import classes from "./SideDrawer.module.css";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Aux";
+// import { checkPropTypes } from "prop-types";
+
+const sideDrawer = (props) => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
+  return (
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(" ")}>
+        <Logo height="11%" margin-bottom="32px" />
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
+  );
+};
+
+export default sideDrawer;
